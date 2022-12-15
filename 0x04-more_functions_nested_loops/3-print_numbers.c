61604107ee6022803459ae613950b703f235f31a
ahmedkatoon <ahmedkatoon1@gmail.com>
2022-12-15 05:18:16 -0800

    tttask 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print num from 0 to 9
+ * print_numbers - print num from 0-9
  * Return: void
  */
 void print_numbers(void)
 {
 	char c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
