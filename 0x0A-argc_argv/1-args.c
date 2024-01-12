3e9e29a5fc8ffdca4d1078920aa81c362d9971ec
ahmedkatoon <ahmedkatoon1@gmail.com>
2024-01-12 04:20:49 -0800

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - function prints number of arguments passed into it
+ *@argc: arguments counter
+ *@argv: arguments vector
+ *Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", argc - 1);
+
+
+return (0);
+
+}
